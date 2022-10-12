7885c8879898c627fc8b3deffb92b9d45031abc8
DoradSoft <doradsoft@gmail.com>
2022-10-12 12:51:17 +0300

    debouncing _onDragUpdate

--- lib/src/widget/book_state.dart
+++ lib/src/widget/book_state.dart
@@ -8,14 +8,16 @@ import 'package:flip_book/src/widgets_ext/multi_animated_builder.dart';
 import 'dart:math';
 
 enum Direction { backward, forward }
+
 const fastDx = 500;
 
-class FlipBookState extends State<FlipBook>
-    with TickerProviderStateMixin, AutomaticKeepAliveClientMixin<FlipBook> {
+class FlipBookState extends State<FlipBook> with TickerProviderStateMixin, AutomaticKeepAliveClientMixin<FlipBook> {
   Leaf? currentLeaf;
   Size _bgSize = const Size(0, 0);
   Direction? _direction;
   double _delta = 0;
+  bool _dragUpdating = false;
+  final Map<int, bool> _leafBuilding = {};
   Size _coverSize = const Size(0, 0);
   Size _leafSize = const Size(0, 0);
   late double _startingPos;
@@ -33,22 +35,16 @@ class FlipBookState extends State<FlipBook>
   Widget build(BuildContext context) {
     super.build(context);
     final controller = widget.controller;
+
     return LayoutBuilder(builder: (context, constraints) {
       _bgSize = Size(constraints.maxWidth, constraints.maxHeight);
-      final maxCoverSize = Size((_bgSize.width - widget.padding.horizontal) / 2,
-          _bgSize.height - widget.padding.vertical);
+      final maxCoverSize =
+          Size((_bgSize.width - widget.padding.horizontal) / 2, _bgSize.height - widget.padding.vertical);
       _coverSize = maxCoverSize.aspectRatio > widget.coverAspectRatio.value
-          ? Size(maxCoverSize.height * widget.coverAspectRatio.value,
-              maxCoverSize.height)
-          : Size(maxCoverSize.width,
-              maxCoverSize.width / widget.coverAspectRatio.value);
-      _leafSize = Size(
-          _coverSize.width *
-              widget.leafAspectRatio.widthFactor /
-              widget.coverAspectRatio.widthFactor,
-          _coverSize.height *
-              widget.leafAspectRatio.heightFactor /
-              widget.coverAspectRatio.heightFactor);
+          ? Size(maxCoverSize.height * widget.coverAspectRatio.value, maxCoverSize.height)
+          : Size(maxCoverSize.width, maxCoverSize.width / widget.coverAspectRatio.value);
+      _leafSize = Size(_coverSize.width * widget.leafAspectRatio.widthFactor / widget.coverAspectRatio.widthFactor,
+          _coverSize.height * widget.leafAspectRatio.heightFactor / widget.coverAspectRatio.heightFactor);
       return Directionality(
         textDirection: widget.direction,
         child: Material(
@@ -57,30 +53,24 @@ class FlipBookState extends State<FlipBook>
             onHorizontalDragUpdate: _onDragUpdate,
             onHorizontalDragEnd: _onDragEnd,
             child: MultiAnimatedBuilder(
-                animations:
-                    controller.leaves.map((leaf) => leaf.animationController),
-                builder: (_, __) => Stack(
-                        clipBehavior: Clip.none,
-                        fit: StackFit.expand,
+                animations: controller.leaves.map((leaf) => leaf.animationController),
+                builder: (_, __) => Stack(clipBehavior: Clip.none, fit: StackFit.expand, children: [
+                      Container(color: const Color(0xff515151)),
+                      Stack(
                         children: [
-                          Container(color: const Color(0xff515151)),
-                          Stack(
-                            children: [
-                              ...controller.leaves
-                                  .where((leaf) =>
-                                      leaf.animationController.value > 0.5)
-                                  .map((leaf) => leafBuilder(context, leaf))
-                            ],
-                          ),
-                          Stack(
-                            children: [
-                              ...controller.leaves.reversed
-                                  .where((leaf) =>
-                                      leaf.animationController.value < 0.5)
-                                  .map((leaf) => leafBuilder(context, leaf)),
-                            ],
-                          ),
-                        ])),
+                          ...controller.leaves
+                              .where((leaf) => leaf.animationController.value > 0.5)
+                              .map((leaf) => leafBuilder(context, leaf))
+                        ],
+                      ),
+                      Stack(
+                        children: [
+                          ...controller.leaves.reversed
+                              .where((leaf) => leaf.animationController.value < 0.5)
+                              .map((leaf) => leafBuilder(context, leaf)),
+                        ],
+                      ),
+                    ])),
           ),
         ),
       );
@@ -88,48 +78,49 @@ class FlipBookState extends State<FlipBook>
   }
 
   Widget leafBuilder(BuildContext context, Leaf leaf) {
-    if ((!leaf.isCover &&
-            (leaf.index - controller.currentLeaf.index).abs() >
-                widget.bufferSize) ||
-        (leaf.isCover &&
-            widget.coverAspectRatio.value == widget.leafAspectRatio.value))
-      return const SizedBox.shrink();
-    final animationVal = leaf.animationController.value;
-    final firstPageTransformed = Transform(
-        alignment: Alignment.center,
-        transform: Matrix4.rotationY(-pi),
-        child: widget.pageDelegate.build(context, _leafSize, leaf.pages.first));
-    final lastPage =
-        widget.pageDelegate.build(context, _leafSize, leaf.pages.last);
-    List<Widget> pages = [firstPageTransformed, lastPage];
-    final pageMaterial = Align(
-        alignment: isLTR ? Alignment.centerRight : Alignment.centerLeft,
-        child: SizedBox(
-            height: leaf.isCover ? _coverSize.height : _leafSize.height,
-            width: leaf.isCover ? _coverSize.width : _leafSize.width,
-            child: Stack(
-                clipBehavior: Clip.antiAliasWithSaveLayer,
-                children:
-                    animationVal < 0.5 ? pages.reversed.toList() : pages)));
-    return Positioned.fill(
-      top: widget.padding.top,
-      bottom: widget.padding.bottom,
-      left: (isLTR ? _coverSize.width : 0) + widget.padding.left,
-      right: (isLTR ? 0 : _coverSize.width) + widget.padding.right,
-      child: Transform.translate(
-        offset: Offset(_coverSize.width, 0),
-        child: Transform(
-          transform: Matrix4.identity()..rotateY(isLTR ? -pi : pi),
+    _leafBuilding[leaf.index] = true;
+    try {
+      if ((!leaf.isCover && (leaf.index - controller.currentLeaf.index).abs() > widget.bufferSize) ||
+          (leaf.isCover && widget.coverAspectRatio.value == widget.leafAspectRatio.value)) {
+        return const SizedBox.shrink();
+      }
+      final animationVal = leaf.animationController.value;
+      final firstPageTransformed = Transform(
+          alignment: Alignment.center,
+          transform: Matrix4.rotationY(-pi),
+          child: widget.pageDelegate.build(context, _leafSize, leaf.pages.first));
+      final lastPage = widget.pageDelegate.build(context, _leafSize, leaf.pages.last);
+      List<Widget> pages = [firstPageTransformed, lastPage];
+      final pageMaterial = Align(
+          alignment: isLTR ? Alignment.centerRight : Alignment.centerLeft,
+          child: SizedBox(
+              height: leaf.isCover ? _coverSize.height : _leafSize.height,
+              width: leaf.isCover ? _coverSize.width : _leafSize.width,
+              child: Stack(
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  children: animationVal < 0.5 ? pages.reversed.toList() : pages)));
+      return Positioned.fill(
+        top: widget.padding.top,
+        bottom: widget.padding.bottom,
+        left: (isLTR ? _coverSize.width : 0) + widget.padding.left,
+        right: (isLTR ? 0 : _coverSize.width) + widget.padding.right,
+        child: Transform.translate(
+          offset: Offset(_coverSize.width, 0),
           child: Transform(
-            transform: Matrix4.identity()
-              ..setEntry(3, 2, 0.001)
-              ..rotateY((isLTR ? -pi : pi) * animationVal),
-            alignment: isLTR ? Alignment.centerRight : Alignment.centerLeft,
-            child: pageMaterial,
+            transform: Matrix4.identity()..rotateY(isLTR ? -pi : pi),
+            child: Transform(
+              transform: Matrix4.identity()
+                ..setEntry(3, 2, 0.001)
+                ..rotateY((isLTR ? -pi : pi) * animationVal),
+              alignment: isLTR ? Alignment.centerRight : Alignment.centerLeft,
+              child: pageMaterial,
+            ),
           ),
         ),
-      ),
-    );
+      );
+    } finally {
+      _leafBuilding[leaf.index] = false;
+    }
   }
 
   void _onDragStart(DragStartDetails details) {
@@ -142,48 +133,49 @@ class FlipBookState extends State<FlipBook>
   }
 
   void _onDragUpdate(DragUpdateDetails details) {
-    if (controller.animating) {
+    if (controller.animating || _dragUpdating || _leafBuilding.containsValue(true)) {
       return;
     }
-    _delta = isLTR
-        ? _startingPos - details.globalPosition.dx
-        : details.globalPosition.dx - _startingPos;
-    if (_delta.abs() > _bgSize.width) return;
-    if (_delta == 0) return;
-    _direction =
-        _direction ?? ((_delta > 0) ? Direction.forward : Direction.backward);
-    switch (_direction!) {
-      case Direction.forward:
-        final pos = _delta / _bgSize.width;
-        // drag overflow
-        if (pos > 1 || _delta < 0) {
-          return;
-        }
-        if (currentLeaf == null) {
-          if (controller.isClosedInverted) {
+    _dragUpdating = true;
+    try {
+      _delta = isLTR ? _startingPos - details.globalPosition.dx : details.globalPosition.dx - _startingPos;
+      if (_delta.abs() > _bgSize.width) return;
+      if (_delta == 0) return;
+      _direction = _direction ?? ((_delta > 0) ? Direction.forward : Direction.backward);
+      switch (_direction!) {
+        case Direction.forward:
+          final pos = _delta / _bgSize.width;
+          // drag overflow
+          if (pos > 1 || _delta < 0) {
             return;
-          } else {
-            currentLeaf = controller.currentOrTurningLeaves.item2!;
           }
-        }
-        controller.currentLeaf.animationController.value = pos;
-        break;
-      case Direction.backward:
-        // reverse
-        final pos = 1 - (_delta.abs() / _bgSize.width);
-        // drag overflow
-        if (pos < 0 || _delta > 0) {
-          return;
-        }
-        if (currentLeaf == null) {
-          if (controller.isClosed) {
+          if (currentLeaf == null) {
+            if (controller.isClosedInverted) {
+              return;
+            } else {
+              currentLeaf = controller.currentOrTurningLeaves.item2!;
+            }
+          }
+          controller.currentLeaf.animationController.value = pos;
+          break;
+        case Direction.backward:
+          // reverse
+          final pos = 1 - (_delta.abs() / _bgSize.width);
+          // drag overflow
+          if (pos < 0 || _delta > 0) {
             return;
-          } else {
-            currentLeaf = controller.currentOrTurningLeaves.item1!;
           }
-        }
-        controller.currentOrTurningLeaves.item1!.animationController.value =
-            pos;
+          if (currentLeaf == null) {
+            if (controller.isClosed) {
+              return;
+            } else {
+              currentLeaf = controller.currentOrTurningLeaves.item1!;
+            }
+          }
+          controller.currentOrTurningLeaves.item1!.animationController.value = pos;
+      }
+    } finally {
+      _dragUpdating = false;
     }
   }
 
@@ -198,16 +190,14 @@ class FlipBookState extends State<FlipBook>
     final turningLeafAnimCtrl = currentLeaf!.animationController;
     switch (_direction!) {
       case Direction.forward:
-        if (((isLTR ? pps.dx < -fastDx : pps.dx > fastDx) ||
-            turningLeafAnimCtrl.value >= 0.5)) {
+        if (((isLTR ? pps.dx < -fastDx : pps.dx > fastDx) || turningLeafAnimCtrl.value >= 0.5)) {
           animate = turningLeafAnimCtrl.forward;
         } else {
           animate = turningLeafAnimCtrl.reverse;
         }
         break;
       case Direction.backward:
-        if (((isLTR ? pps.dx > fastDx : pps.dx < -fastDx) ||
-            turningLeafAnimCtrl.value <= 0.5)) {
+        if (((isLTR ? pps.dx > fastDx : pps.dx < -fastDx) || turningLeafAnimCtrl.value <= 0.5)) {
           animate = turningLeafAnimCtrl.reverse;
         } else {
           animate = turningLeafAnimCtrl.forward;
